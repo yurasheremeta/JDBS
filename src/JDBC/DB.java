016e1861219db611f23cfd5c820bf6f0c1219642
yurasheremeta <sheremeta.yura@gmail.com>
2018-07-17 15:08:24 +0300

    Added connecting to db and now we  can create country , city and human in our db

---
+++ src/JDBC/DB.java
@@ -0,0 +1,107 @@
+package JDBC;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DB  {
+    protected static void Create_Tables() throws SQLException{
+        String delete_country = "drop table if exists country;";
+        String create_country = "create table country (" +
+                "id int not null primary key auto_increment," +
+                "country_name varchar(30)" +
+                ");";
+        String delete_city = "drop table if exists city;";
+        String create_city = "create table city(" +
+                "id int not null primary key auto_increment," +
+                "city_name varchar(30)," +
+                "country_id int" +
+                ");";
+        String delete_human = "drop table if exists person";
+        String create_human = "create table person(" +
+                "id int NOT NULL primary key auto_increment," +
+                "first_name varchar(30), " +
+                "last_name varchar(30)," +
+                "age int, " +
+                "hobby varchar(30)," +
+                "city_id int" +
+                ");";
+        Statement stm = Program.conn.createStatement();
+        stm.execute(delete_human);
+        stm.execute(create_human);
+
+
+
+        stm.execute(delete_city);
+        stm.execute(create_city);
+        stm.execute(delete_country);
+        stm.execute(create_country);
+
+
+
+        stm.close();
+    }
+    protected static void alter_tables()throws SQLException{
+        String country_city = "alter table city add foreign key (country_id)" +
+                "references country(id); ";
+        String person_city = "alter table person add foreign key (city_id) " +
+                "references city(id);";
+
+        Statement stm = Program.conn.createStatement();
+
+        stm.execute(country_city);
+        stm.execute(person_city);
+
+
+        stm.close();
+    }
+    protected static void addCountry()throws SQLException{
+        Scanner scan = new Scanner(System.in);
+        String query = "Insert into country(country_name) values(?)";
+        PreparedStatement ps = Program.conn.prepareStatement(query);
+        String a = scan.next();
+        ps.setString(1,a);
+        ps.executeUpdate();
+        ps.close();
+        System.out.println("Country aded!");
+    }
+    protected static void addCity()throws SQLException{
+        Scanner scan = new Scanner(System.in);
+        String query = "Insert into city(city_name , country_id) values(?,?)";
+        PreparedStatement ps = Program.conn.prepareStatement(query);
+        String a = scan.next();
+        int country_id = scan.nextInt();
+        ps.setString(1,a);
+        ps.setInt(2,country_id);
+        ps.executeUpdate();
+        ps.close();
+        System.out.println("City aded!");
+    }
+    protected static void addHuman() throws SQLException{
+        Scanner scan = new Scanner(System.in);
+        String query = "Insert into person(first_name ,last_name , age , hobby , city_id ) values(?,?,?,?,?)";
+        PreparedStatement ps = Program.conn.prepareStatement(query);
+        System.out.println("Enter the name: ");
+        String first_name  = scan.next();
+        System.out.println("Enter the last_name");
+        String last_name = scan.next();
+        System.out.println("Enter age");
+        int age = scan.nextInt();
+        System.out.println("Enter the hobby of " +first_name);
+        String hobby = scan.next();
+        System.out.println("From what city: ");
+        int city_id = scan.nextInt();
+//        System.out.println("From what city: ");
+//        int city_id = scan.nextInt();
+        ps.setString(1,first_name);
+        ps.setString(2,last_name);
+        ps.setInt(3,age);
+        ps.setString(4,hobby);
+        ps.setInt(5,city_id);
+
+        ps.executeUpdate();
+        ps.close();
+        System.out.println(first_name+" is aded!");
+    }
+}
---
+++ src/JDBC/Program.java
@@ -0,0 +1,56 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Program {
+    static Connection conn;
+    public static void main(String[] args) throws SQLException {
+        String dbURL = "jdbc:mysql://localhost:3306/human_db_2?useSSL=false";
+        String userName = "root";
+        String password = "root12345";
+        conn = DriverManager.getConnection(dbURL, userName, password);
+        System.out.println("Connecting: " + !conn.isClosed());
+        Scanner scan = new Scanner(System.in);
+
+        DB.Create_Tables();
+        DB.alter_tables();
+
+        while(true){
+
+            printMenu();
+            String fromUser = scan.next();
+            switch(fromUser){
+                case "1":
+                    DB.addCountry();
+                    break;
+                case "2":
+                    DB.addCity();
+                    break;
+                case "3":
+                    DB.addHuman();
+                default:
+                    conn.close();
+            }
+        }
+    }
+
+    private static void printMenu(){
+        System.out.println("1 - Add country");
+        System.out.println("2 - Add city");
+        System.out.println("21 - add Country_City");
+        System.out.println("3 - Add person");
+        System.out.println("31 - Add person_city");
+        System.out.println("4 - Output all information from city");
+        System.out.println("41 - Output all information from Country_city");
+        System.out.println("5 - Output all information from country");
+        System.out.println("6 - Output all information from person");
+        System.out.println("7 - Output person by id");
+        System.out.println("8 - Output city by id");
+        System.out.println("9 - Output country by id");
+        System.out.println("10 - Output information about people in the same city");
+        System.out.println("11 - join Country_city");
+    }
+}
